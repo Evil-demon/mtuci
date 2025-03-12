23b14a22f9f0ef48e93cb8b259b1400af0c42ccc
Evil <61279634+Evil-demon@users.noreply.github.com>
2025-03-12 09:49:36 +0300

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,19 +1,24 @@
 # Simple Calculator
 
+
 def addition(x, y):
     return x + y
+
 
 def subtraction(x, y):
     return x - y
 
+
 def multiplication(x, y):
     return x * y
+
 
 def division(x, y):
     if y != 0:
         return x / y
     else:
         return "Error: division by zero"
+
 
 # Main code
 
